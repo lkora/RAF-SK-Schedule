f7d0dc1f6df267a5f5987ce75c87a020f397069e
djues3 <djues3@gmail.com>
2023-11-22 01:45:45 +0100

    Update filtering logic and introduce schedule exclusions
    Updated the ProfessorFilter to include partial matches, enabling more flexible schedule filtering. Introduced the Exclusion interface with a basic HolidayExclusion implementation. This will allow schedules to exclude certain dates or events, improving schedule flexibility. Also, removed unused code in the Classroom class.

--- Schedule/src/main/java/schedule/filter/ProfessorFilter.java
+++ Schedule/src/main/java/schedule/filter/ProfessorFilter.java
@@ -2,9 +2,16 @@ package schedule.filter;
 
 import schedule.lecture.Lecture;
 
+/**
+ * Represents a filter that checks if a lecture's professor matches a requirement.
+ * <p>
+ *     Returns true if the lecture's professor contains the requirement.
+ * <p>
+ * Implements the Filter interface.
+ */
 public record ProfessorFilter(String requirement) implements Filter {
 	@Override
 	public boolean test(Lecture lecture) {
-		return lecture.getProfessor().equals(requirement);
+		return lecture.getProfessor().toLowerCase().contains(requirement.toLowerCase());
 	}
 }
