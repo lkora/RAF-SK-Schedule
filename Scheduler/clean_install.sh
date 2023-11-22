5a23db5e8f5c5eeee77b8208754baaa3da7d481c
djues3 <djues3@gmail.com>
2023-11-22 12:52:06 +0100

    Add clean_install.sh script for simplifying module builds
    This commit introduces clean_install.sh, a script constructed to simplify the process of building each of our main submodules. It works by iterating over a defined list of modules, changing into their respective directories, and running 'mvn clean install'. This should help automate the often tedious process of manually building each module.

---
+++ Scheduler/clean_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Remember the root directory
+root_directory=$(pwd)
+command_to_run='mvn clean install'
+
+# Building list of modules
+declare -a modules=("Lecture" "ScheduleManagerService/ScheduleImporterSpec" "ScheduleManagerService/ScheduleExporterSpec" "ScheduleManagerService" "ScheduleManager/ScheduleManagerCollection" "ScheduleManager" "Schedule")
+
+# Build all modules first
+for module in "${modules[@]}"; do
+    echo "Running $command_to_run on module $module"
+    # Navigate to the module directory
+    cd $root_directory/$module
+    # Run the command
+    $command_to_run
+done
